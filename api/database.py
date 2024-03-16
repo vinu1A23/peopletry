545a58c4969f0103ba5a141e969e29f0c56a10ec
vinu1A23 <tan2002vinu@gmail.com>
2024-03-16 20:33:10 +0530

    updating main to app

--- api/database.py
+++ api/database.py
@@ -2,7 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm.decl_api import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os 
-SQLALCHEMY_DATABASE_URL = os.environ["POSTGRES_URL"]
+if os.getcwd()[-24:]=='a_analysis\\peopletry\\api':
+    SQLALCHEMY_DATABASE_URL= os.environ["LOCAL_POSTGRES_URL"]
+    print(SQLALCHEMY_DATABASE_URL)
+else:
+    SQLALCHEMY_DATABASE_URL = os.environ["POSTGRES_URL"]
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
