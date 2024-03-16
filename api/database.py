6189bdae96a1127b94db4bf3c99b35862b72f32f
vinu1A23 <tan2002vinu@gmail.com>
2024-03-16 18:13:11 +0530

    updating database.py

--- api/database.py
+++ api/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm.decl_api import declarative_base
 from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:curiouspigonabike@localhost:5432/task?connect_timeout=10&sslmode=prefer"
+import os 
+SQLALCHEMY_DATABASE_URL = os.environ["POSTGRES_URL"]
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
