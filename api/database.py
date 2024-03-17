2ea86db1394bafb09cdf5cc9f7baf6fd9326b105
vinu1A23 <tan2002vinu@gmail.com>
2024-03-17 16:11:09 +0530

    Update database.py

--- api/database.py
+++ api/database.py
@@ -2,11 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm.decl_api import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os 
-if os.getcwd()[-1:]=='i':
-    SQLALCHEMY_DATABASE_URL= os.environ["LOCAL_POSTGRES_URL"]
-    print(SQLALCHEMY_DATABASE_URL)
-else:
-    SQLALCHEMY_DATABASE_URL = os.environ["POSTGRES_URL"]
+SQLALCHEMY_DATABASE_URL = "postgres://default:8t5QxvHanDpC@ep-aged-pond-a2motml2-pooler.eu-central-1.aws.neon.tech/verceldb?sslmode=require" 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base() 
