c7f5e87a185ac751326d67e1dea5bd13ac0a9b61
vinu1A23 <tan2002vinu@gmail.com>
2024-03-17 08:55:47 +0530

    Update database.py

--- api/database.py
+++ api/database.py
@@ -9,4 +9,4 @@ else:
     SQLALCHEMY_DATABASE_URL = os.environ["POSTGRES_URL"]
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+Base = declarative_base() 
